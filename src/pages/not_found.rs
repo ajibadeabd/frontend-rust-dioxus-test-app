3143f757598697467cd94aa65b9d5ee752dff074
ajibadeabd <43596899+ajibadeabd@users.noreply.github.com>
2023-08-22 10:41:45 +0100

    added axum

--- src/pages/not_found.rs
+++ src/pages/not_found.rs
@@ -2,7 +2,7 @@ use dioxus::prelude::*;
 
 #[inline_props]
 pub fn NotFound(cx: Scope,segments: Vec<String> ) -> Element {
-    println!("{:?}",segments);
+    // println!("{:?}",segments);
     render!(rsx!(
         div{
             "not found"
